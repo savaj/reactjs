147cd14801e646693bfa1872ce39c5f7506d8449
savaj.patel <savaj.patel@solutionanalysts.com>
2020-03-25 11:35:49 +0530

    initial commit

---
+++ src/Person/Person.js
@@ -0,0 +1,20 @@
+ import React from 'react';
+ import './Person.css';
+
+ const person = (props) => {
+    
+    return (
+        <div className="Person">
+            <p onClick={props.click}>HI, I'm {props.name}. I am  {props.age} years old</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+        
+    );
+ 
+ 
+ }
+
+
+ export default person;
+
